5ba7d10ff1fee24a9d21e53f4a62a7020a9f49cf
haodudecao <34431537+haodudecao@users.noreply.github.com>
2020-03-12 00:31:24 +0800

    Update isPalindrome.php

--- 009. isPalindrome/isPalindrome.php
+++ 009. isPalindrome/isPalindrome.php
@@ -8,4 +8,20 @@ class Solution {
     function isPalindrome($x) {
         return $x==strrev($x);
     }
+    
+    /**
+     * @param Integer $x
+     * @return Boolean
+     */
+    function isPalindrome($x) {
+        if ($x < 0 || ($x % 10 == 0 && $x != 0)) {
+            return false;
+        }
+        $num = 0;
+        while ($x > $num) {
+            $num = $num * 10 + ($x % 10);
+            $x = floor($x / 10);
+        }
+        return $x == floor($num / 10) || ($x == $num);
+    }
 }
